00e8bf342780a39abbbc87369652d96f7cf716e4
Mara2345 <rafa_mancina@hotmail.com>
2024-02-01 10:05:12 +0100

    second commit

--- obj/Debug/net6.0-windows/PaginaProductos.g.i.cs
+++ obj/Debug/net6.0-windows/PaginaProductos.g.i.cs
@@ -19,6 +19,7 @@ using System.Windows.Controls.Primitives;
 using System.Windows.Controls.Ribbon;
 using System.Windows.Data;
 using System.Windows.Documents;
+using System.Windows.Forms.Integration;
 using System.Windows.Ink;
 using System.Windows.Input;
 using System.Windows.Markup;
@@ -69,7 +70,7 @@ namespace CRUDInterfaces {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/CRUDInterfaces;V1.0.0.0;component/paginaproductos.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/CRUDInterfaces;component/paginaproductos.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\PaginaProductos.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
